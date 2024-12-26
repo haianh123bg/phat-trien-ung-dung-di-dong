21d02b025b7b21ef9f38685ac631d08871cc0557
haianh123bg <nguyenngochaianh20033@gmail.com>
2024-12-26 22:27:43 +0700

    t

--- BTL/app/src/main/java/com/nguyenngochaianh/btl/db/DatabaseHelper.java
+++ BTL/app/src/main/java/com/nguyenngochaianh/btl/db/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "CarCare.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     public static final String TABLE_USER = "Users";
     public static final String TABLE_CAR = "Cars";
